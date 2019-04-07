ccfee734ae9c128f04b81a6dbe150babe84bc208
sk <sskenner@gmail.com>
2019-04-07 17:37:31 -0400

    Working With The ReadLine Module

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
-const EventEmitter = require('events');
-const eventEmitter = new EventEmitter();
+const readline = require('readline');
+const rl = readline.createInterface({ input : process.stdin,
+                                     output : process.stdout });
+let num1 = Math.floor((Math.random() * 10) +1);
+let num2 = Math.floor((Math.random() * 10) +1);
+let answer = num1 + num2;
 
-eventEmitter.on('tutorial',(num1, num2)=>{
-    console.log(num1 + num2);
+rl.question(`what is ${ num1 } + ${ num2 }?\n`,
+    (userInput)=>{ 
+        if(userInput.trim() == answer){
+            rl.close()
+        }
+        else{
+            rl.setPrompt('Incorrect.try again:\n');
+            rl.prompt();
+            rl.on('line',(userInput)=>{
+                if(userInput.trim() == answer){
+                    rl.close();
+                }
+                else{
+                    rl.setPrompt(`answer ${ userInput } incorrect. try again:\n`);
+                    rl.prompt();
+                }
+            })
+        }
+    }); 
+
+rl.on('close',()=>{
+    console.log('Die-is-it!!');
 });
-
-eventEmitter.emit('tutorial',1,2);
-
-class Person extends EventEmitter{
-    constructor(name){
-        super();
-        this._name = name;
-    }
-
-    get name(){
-        return this._name;
-    }
-}
-
-let pedro = new Person('Pedro');
-let christina = new Person('Christina');
-christina.on('name',()=>{
-    console.log('my name is ' + christina.name);
-});
-pedro.on('name',()=>{
-    console.log('my name is ' + pedro.name);
-});
-
-pedro.emit('name');
-christina.emit('name');
