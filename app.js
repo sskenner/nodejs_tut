c6f5c921f4d4719e765f8c2e782cb7e4c08483d6
sk <sskenner@gmail.com>
2019-04-07 13:28:13 -0400

    update

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-const tutorial = require('./tutorial');
-console.log(tutorial);
-console.log(tutorial.sum(1,1));
-console.log(tutorial.PI);
-console.log(new tutorial.SomeMathObject());
+const EventEmitter = require('events');
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on('tutorial',()=>{
+    console.log('tutorial event has occured');
+});
+
+eventEmitter.emit('tutorial');
