7104f5b7c1e5815ee260a9dfa2ac3ffc49224c19
sk <sskenner@gmail.com>
2019-04-07 20:54:12 -0400

    ⌨️ (1:05:41) Pipes and Pipe Chaining. (Readable,Writable and Transform Streams)

--- app.js
+++ app.js
@@ -5,15 +5,3 @@ const readStream = fs.createReadStream('./example2.txt.gz');
 const writeStream = fs.createWriteStream('uncompressed.txt');
 readStream.pipe(gunzip).pipe(writeStream);
 
-// const fs = require('fs');
-// const zlib = require('zlib');
-// const gzip = zlib.createGzip();
-// const readStream = fs.createReadStream('./example.txt','utf8');
-// const writeStream = fs.createWriteStream('example2.txt.gz');
-// // readStream.on('data',(chunk)=>{
-// //     writeStream.write(chunk);
-// // });
-
-// // readStream.pipe(writeStream);
-// readStream.pipe(gzip).pipe(writeStream);
-
