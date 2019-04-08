31845b33865a947f0f048b20120e766e0d718862
sk <sskenner@gmail.com>
2019-04-07 20:51:51 -0400

    ⌨️ (1:02:40) Why you should use Streams

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const fs = require('fs');
-const readStream = fs.createReadStream('./example.txt');
-const writeStream = fs.createWriteStream('example2.txt');
-readStream.on('data',(chunk)=>{
-    writeStream.write(chunk);
-});
+const zlib = require('zlib');
+const gunzip = zlib.createGunzip();
+const readStream = fs.createReadStream('./example2.txt.gz');
+const writeStream = fs.createWriteStream('uncompressed.txt');
+readStream.pipe(gunzip).pipe(writeStream);
+
+// const fs = require('fs');
+// const zlib = require('zlib');
+// const gzip = zlib.createGzip();
+// const readStream = fs.createReadStream('./example.txt','utf8');
+// const writeStream = fs.createWriteStream('example2.txt.gz');
+// // readStream.on('data',(chunk)=>{
+// //     writeStream.write(chunk);
+// // });
+
+// // readStream.pipe(writeStream);
+// readStream.pipe(gzip).pipe(writeStream);
+
