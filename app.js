dff6147d43c49e68ec78fc054224cf60f1430aed
sk <sskenner@gmail.com>
2019-04-08 19:23:05 -0400

    ⌨️ (1:17:52) Serving Static Files with Http and File System Module (html,json,image)

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
 const http = require('http');
-const server = http.createServer((req,res)=>{
-    if(req.url === '/'){
-        res.write('sup');
-        res.end();
-    }
-    else {
-        res.write('some other');
-        res.end();
-    }
-});
-
-server.listen('3000');
+const fs = require('fs');
+http.createServer((req,res)=>{
+    const readStream = fs.createReadStream('./static/example.png');
+    res.writeHead(200,{'Content-type': 'image/png'});
+    readStream.pipe(res);
+}).listen('3000');
