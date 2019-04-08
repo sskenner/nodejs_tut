92e8e2c4f1f0fab0dc7dc2e2ea898ccb6136889f
sk <sskenner@gmail.com>
2019-04-08 19:44:04 -0400

    ⌨️ (1:27:18) Installing Packages using Npm (Node Package Manager)

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-const http = require('http');
-const fs = require('fs');
-http.createServer((req,res)=>{
-    const readStream = fs.createReadStream('./static/example.png');
-    res.writeHead(200,{'Content-type': 'image/png'});
-    readStream.pipe(res);
-}).listen('3000');
+const _ = require('lodash');
+let example = _.fill([1,2,3,4,5],"banana",1,4);
+console.log(example);
