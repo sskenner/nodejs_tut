b25bcd1bf3d4a8eb0be606c8a7cf04926c4e5699
skA <sskenner@gmail.com>
2019-05-30 16:55:33 -0400

    add goto home & new call urls in tabs

--- 052719/esdPup/esd.js
+++ 052719/esdPup/esd.js
@@ -1,13 +1,18 @@
+require('dotenv').config();
 const puppeteer = require('puppeteer');
 
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto('https://nychhcuat.service-now.com/');
+  const page_home = await browser.newPage();
+  await page_home.authenticate({username: process.env.USR, password: process.env.PSS});
+  // await page.authenticate({username: "kenners", password: "$%RTfgvb0318"});
+  await page_home.goto('https://nychhcuat.service-now.com/');
+  const page_call = await browser.newPage();
+  await page_call.goto('https://nychhcuat.service-now.com/new_call.do?sys_id=-1&sysparm_stack=new_call_list.do');
 
-//   await page.screenshot({path: 'example.png'});
+// //   await page.screenshot({path: 'example.png'});
 
-//   await browser.close();
+// //   await browser.close();
 })();
 
 // new call - https://nychhcuat.service-now.com/new_call.do?sys_id=-1&sysparm_stack=new_call_list.do
