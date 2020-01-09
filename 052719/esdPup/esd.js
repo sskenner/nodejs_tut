fee6597b3ca8116097de961a144977da86ebd92a
skA <sskenner@gmail.com>
2020-01-09 04:24:04 -0500

    update before delete from aria

--- 052719/esdPup/esd.js
+++ 052719/esdPup/esd.js
@@ -4,11 +4,13 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page_home = await browser.newPage();
+  await page_home.setViewport({ width: 1280, height: 800 });
   await page_home.authenticate({username: process.env.USR, password: process.env.PSS});
   // await page.authenticate({username: "kenners", password: "$%RTfgvb0318"});
   await page_home.goto('https://nychhcuat.service-now.com/');
   const page_call = await browser.newPage();
-  await page_call.goto('https://nychhcuat.service-now.com/new_call.do?sys_id=-1&sysparm_stack=new_call_list.do');
+  await page_call.setViewport({ width: 1280, height: 800 });
+  await page_call.goto('https://nychhcuat.service-now.com/new_call.do?sys_id=-1&sysparm_stack=new_call_list.do', { waitUntil: 'networkidle2' });
 
 // //   await page.screenshot({path: 'example.png'});
 
